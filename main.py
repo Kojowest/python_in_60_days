e5288e29ad702f4c8b31f2dc09052182a6d9694e
Kojowest <kojowest2012@gmail.com>
2023-11-01 02:31:11 +0000

    Bonus 5 added with several features added to the cli to do app

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, edit or exit: ")
+    user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -9,13 +9,16 @@ while True:
             todo = input("Enter a todo: ")
             todos.append(todo)
         case 'show':
-            for item in todos:
-                print(item)
+            for index, item in enumerate(todos):
+                print(f"{index + 1}-{item}")
         case 'edit':
             number = int(input("Number of the todo to edit: "))
             number = number - 1
             new_todo = input("Enter a new todo: ")
             todos[number] = new_todo
+        case 'complete':
+            number = int(input("Number of todo to complete: "))
+            todos.pop(number -1)
         case 'exit':
             break
         case _:
